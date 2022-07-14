dfc2db71a710d1945624cc8399866ee9c5f18a5c
Silvia Zveiter de Albuquerque Rocha <silviaz@camara.nf>
2022-07-14 18:32:31 -0300

    falta habilitar o botão quando tudo for preenchido

---
+++ app.js
@@ -0,0 +1,35 @@
+// habilita o botão do formulário somente se todos os campos forem preenchidos //
+
+// se houver pelo menos um campo vazio, retorna false //
+function checkInputs(inputs) {
+    var filled = true;
+    
+    inputs.forEach(function(input) {
+        
+      if(input.value === "") {
+          filled = false;
+      }
+    
+    });
+    console.log('filled:',filled);
+    return filled;
+    
+  }
+ 
+  // a constante 'inputs' vai conter todos os inputs do HTML //
+  const inputs = document.querySelectorAll('input');
+  var button = document.querySelector("button");
+  // para cada campo do formulário, adiciona um event listener keyup com a função que habilita //
+  // o botão quando todos os campos estiverem preenchidos //
+
+  inputs.forEach(function(input) {
+    console.log(input);
+    input.addEventListener("keyup", function() {
+        console.log(input.onkeyup);
+        if(checkInputs(inputs)) {
+        button.disabled = false;
+      } else {
+        button.disabled = true;
+      }
+    });
+  });
